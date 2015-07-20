87536457bcee862970a871031d0ed59a1cc118ba
David Gilbertson <gilbertson.david@gmail.com>
2015-07-20 17:21:24 +1000

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,81 @@
+/*! React Starter Kit | MIT License | http://www.reactstarterkit.com/ */
+
+import React, { PropTypes } from 'react';
+import styles from './App.less';
+import withContext from '../../decorators/withContext';
+import withStyles from '../../decorators/withStyles';
+import AppActions from '../../actions/AppActions';
+import AppStore from '../../stores/AppStore';
+import Header from '../Header';
+import ContentPage from '../ContentPage';
+import ContactPage from '../ContactPage';
+import LoginPage from '../LoginPage';
+import RegisterPage from '../RegisterPage';
+import NotFoundPage from '../NotFoundPage';
+import Feedback from '../Feedback';
+import Footer from '../Footer';
+
+const pages = { ContentPage, ContactPage, LoginPage, RegisterPage, NotFoundPage };
+
+@withContext
+@withStyles(styles)
+class App {
+
+  static propTypes = {
+    path: PropTypes.string.isRequired
+  };
+
+  componentDidMount() {
+    window.addEventListener('popstate', this.handlePopState);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('popstate', this.handlePopState);
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return this.props.path !== nextProps.path;
+  }
+
+  render() {
+    let component;
+
+    switch (this.props.path) {
+
+      case '/':
+      case '/about':
+      case '/privacy':
+        let page = AppStore.getPage(this.props.path);
+        component = React.createElement(pages[page.component], page);
+        break;
+
+      case '/contact':
+        component = <ContactPage />;
+        break;
+
+      case '/login':
+        component = <LoginPage />;
+        break;
+
+      case '/register':
+        component = <RegisterPage />;
+        break;
+    }
+
+    return component ? (
+      <div>
+        <Header />
+        {component}
+        <Feedback />
+        <Footer />
+      </div>
+    ) : <NotFoundPage />;
+  }
+
+  handlePopState(event) {
+    AppActions.navigateTo(window.location.pathname, {replace: !!event.state});
+  }
+
+}
+
+export default App;
---
+++ src/server.js
@@ -0,0 +1,72 @@
+/*! React Starter Kit | MIT License | http://www.reactstarterkit.com/ */
+
+import 'babel/polyfill';
+import _ from 'lodash';
+import fs from 'fs';
+import path from 'path';
+import express from 'express';
+import React from 'react';
+import './core/Dispatcher';
+import './stores/AppStore';
+import db from './core/Database';
+import App from './components/App';
+
+const server = express();
+
+server.set('port', (process.env.PORT || 5000));
+server.use(express.static(path.join(__dirname, 'public')));
+
+//
+// Register API middleware
+// -----------------------------------------------------------------------------
+server.use('/api/query', require('./api/query'));
+
+//
+// Register server-side rendering middleware
+// -----------------------------------------------------------------------------
+
+// The top-level React component + HTML template for it
+const templateFile = path.join(__dirname, 'templates/index.html');
+const template = _.template(fs.readFileSync(templateFile, 'utf8'));
+
+server.get('*', async (req, res, next) => {
+  try {
+    // TODO: Temporary fix #159
+    if (['/', '/about', '/privacy'].indexOf(req.path) !== -1) {
+      await db.getPage(req.path);
+    }
+    let notFound = false;
+    let css = [];
+    let data = {description: ''};
+    let app = (<App
+      path={req.path}
+      context={{
+        onInsertCss: value => css.push(value),
+        onSetTitle: value => data.title = value,
+        onSetMeta: (key, value) => data[key] = value,
+        onPageNotFound: () => notFound = true
+      }} />);
+
+    data.body = React.renderToString(app);
+    data.css = css.join('');
+    let html = template(data);
+    if (notFound) {
+      res.status(404);
+    }
+    res.send(html);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//
+// Launch the server
+// -----------------------------------------------------------------------------
+
+server.listen(server.get('port'), () => {
+  if (process.send) {
+    process.send('online');
+  } else {
+    console.log('The server is running at http://localhost:' + server.get('port'));
+  }
+});
