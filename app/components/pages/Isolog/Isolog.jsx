d3d341b7e80334b7ab2f10c6324d2e109ff461e2
David Gilbertson <gilbertson.david@gmail.com>
2015-07-28 20:38:01 +1000

    Readme loading from github, transitions between screens working.

--- app/components/pages/Isolog/Isolog.jsx
+++ app/components/pages/Isolog/Isolog.jsx
@@ -1,42 +1,42 @@
 import React, {Component, PropTypes} from 'react';
 import {isOnClient} from '../../../utils';
 import marked from 'marked';
-//import {isWebPack} from '../../../utils';
-//
-//if (isWebPack) {
-//    require('./_isolog.scss');
-//}
+import {isWebPack} from '../../../utils';
 
-//function goGet(url) {
-//    let xhr = new XMLHttpRequest();
-//
-//    return new Promise((resolve) => {
-//        xhr.open('GET', url, true);
-//
-//        xhr.onreadystatechange = function () {
-//            if (xhr.readyState != 4 || xhr.status != 200) return;
-//            return resolve(xhr.responseText);
-//        };
-//
-//        xhr.send();
-//    });
-//}
+if (isWebPack) {
+    require('./_isolog.scss');
+}
+
+function goGet(url) {
+    let xhr = new XMLHttpRequest();
+
+    return new Promise((resolve) => {
+        xhr.open('GET', url, true);
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState != 4 || xhr.status != 200) return;
+            return resolve(xhr.responseText);
+        };
+
+        xhr.send();
+    });
+}
 
 class Isolog extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            readme: 'no readme'
+            readme: 'loading...'
         };
     }
 
-    //componentDidMount() {
-    //    if (isOnClient) {
-    //        let readmeUrl = 'https://raw.githubusercontent.com/davidgilbertson/isolog/master/README.md';
-    //
-    //        goGet(readmeUrl).then(readme => this.setState({readme}));
-    //    }
-    //}
+    componentDidMount() {
+        if (isOnClient) {
+            let readmeUrl = 'https://raw.githubusercontent.com/davidgilbertson/isolog/master/README.md';
+
+            goGet(readmeUrl).then(readme => this.setState({readme}));
+        }
+    }
 
     render() {
         return (
