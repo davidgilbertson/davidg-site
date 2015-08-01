310f7404ed3d6b8ea62f32aeef3993335dcc9167
David Gilbertson <gilbertson.david@gmail.com>
2015-08-01 22:02:02 +1000

    V0.0.3 mobile meta tag

--- app/components/Html/Html.jsx
+++ app/components/Html/Html.jsx
@@ -10,6 +10,7 @@ class Html extends Component {
             <html lang="en">
                 <head>
                     <meta charSet="UTF-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
                     <title>{this.props.title || 'David Gilbertson'}</title>
                     <style>{this.props.cssString}</style>
                 </head>
