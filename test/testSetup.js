4e4291ffd20816bb00e799677ec727431a3e6af5
David Gilbertson <gilbertson.david@gmail.com>
2015-08-19 18:33:46 +1000

    Tidy up test setup

--- test/testSetup.js
+++ test/testSetup.js
@@ -4,17 +4,12 @@ import sinonChai from 'sinon-chai';
 
 chai.should();
 chai.use(sinonChai);
-
-// set up the dom
-if (typeof document === 'undefined') {
-    var jsdom = require('jsdom').jsdom;
-
-    global.document = jsdom('<html><body></body></html>');
-    global.window = document.parentWindow;
-    global.navigator = {
-        userAgent: 'node.js'
-    };
-}
-
 global.sinon = sinon;
 global.expect = chai.expect;
+
+//Set up the mock dom
+const jsdom = require('jsdom').jsdom;
+
+global.document = jsdom('<html><body></body></html>');
+global.window = document.parentWindow;
+global.navigator = {userAgent: 'node.js'};
