8d30be1d39b5e2b459b6c03ce305a96932b75472
David Gilbertson <gilbertson.david@gmail.com>
2015-08-23 11:05:21 +1000

    Added Google Analytics snippet

--- app/components/Html/Html.jsx
+++ app/components/Html/Html.jsx
@@ -1,5 +1,13 @@
 import React, {Component, PropTypes} from 'react';
 
+const analyticsSnippet = `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+                          ga('create', 'UA-66646815-1', 'auto');
+                          ga('send', 'pageview');`;
+
 class Html extends Component {
     constructor(props) {
         super(props);
@@ -24,6 +32,8 @@ class Html extends Component {
                         />
 
                     <script src={this.props.scriptFileName}></script>
+
+                    <script dangerouslySetInnerHTML={{__html: analyticsSnippet}}></script>
                 </body>
             </html>
         );
