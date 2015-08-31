7808e09f22327e5941c8ef50e5559a49acdffea5
David Gilbertson <gilbertson.david@gmail.com>
2015-08-31 18:19:02 +1000

    Add compression

--- app/server/server.js
+++ app/server/server.js
@@ -1,16 +1,20 @@
 import path from 'path';
 import renderApp from './renderApp.js';
 import express from 'express';
+import compression from 'compression';
 const server = express();
 const port = process.env.PORT || 80;
 
+server.use(compression());
 server.use(express.static('public'));
 
 server.use('/fireball-js', express.static(path.resolve(__dirname, '../../node_modules/fireball-js')));
 
 server.get('*', renderApp);
 
-server.listen(port);
+server.listen(port, () => {
+    console.log('Node running on port', port);
+});
 
 if (process.env.NODE_ENV !== 'production') {
     require('../../buildTools/webpackDevServer.js');
